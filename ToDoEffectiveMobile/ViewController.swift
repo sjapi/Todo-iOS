23493a00e6faed382b9fc0890d24230492d334cc
sjapi <aguznenkovv@yandex.ru>
2025-07-30 16:31:43 +0300

    Initial commit

---
+++ ToDoEffectiveMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoEffectiveMobile
+//
+//  Created by Arseniy Zolotarev on 30.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
